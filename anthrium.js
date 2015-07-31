c451bc5270a2dd688522141fddedac8964d7246a
Masataka Yakura <masataka.yakura@gmail.com>
2015-07-31 20:17:31 +0900

    rewrite using ES6

---
+++ anthrium.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const annoyingQueryKeys = new Set([
+  'utm_source',
+  'utm_campaign',
+  'utm_medium',
+  'utm_content',
+  'ref',
+  'feature',
+  'fb_action_ids',
+  'context',
+  'ob',
+])
+
+const getSearchParams = queryString => {
+  let queryArray = queryString.slice(1).split('&').map(query => query.split('='))
+  return new Map(queryArray)
+}
+
+const filterSearchParams = queryMap => {
+  let filteredQueries = []
+  queryMap.forEach((value, key) => {
+    if (!annoyingQueryKeys.has(key)) {
+      filteredQueries.push((value !== undefined) ? `${key}=${value}` : key)
+    }
+  })
+  return filteredQueries
+}
+
+const rewriteURL = queryString => {
+  if (!queryString) { return }
+  let filteredQueries = filterSearchParams(getSearchParams(queryString))
+  let newQuery = !!filteredQueries.length ? `${filteredQueries.join('&')}` : ''
+  let newURL = location.origin + location.pathname + newQuery + location.hash
+  history.replaceState(null, '', newURL)
+}
+
+rewriteURL(location.search)
